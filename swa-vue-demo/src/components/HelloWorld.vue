f7b54e6f9b97109bad2ee2c54bb60bf4de79ced5
kathyhana <kathy@yaku.icu>
2024-09-07 07:43:15 +0800

    Update HelloWorld.vue

--- swa-vue-demo/src/components/HelloWorld.vue
+++ swa-vue-demo/src/components/HelloWorld.vue
@@ -9,7 +9,27 @@ const count = ref(0)
 </script>
 
 <template>
+
+<div>
+  <div id="bird" style="width: 100vw; height: 100vh;">
   <h1>{{ msg }}</h1>
+  </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.birds.min.js"></script>
+<script>
+VANTA.BIRDS({
+  el: "#bird",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
+</script></div>
+  
+
 </template>
 
 <style scoped>
