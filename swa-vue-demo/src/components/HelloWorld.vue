934d6b3d365b5e8a8b780894635fdbd8c88310f3
kathyhana <kathy@yaku.icu>
2024-09-07 08:05:15 +0800

    Update HelloWorld.vue

--- swa-vue-demo/src/components/HelloWorld.vue
+++ swa-vue-demo/src/components/HelloWorld.vue
@@ -1,22 +1,10 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js'
+import 'https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.birds.min.js'
 
-defineProps({
-  msg: String,
-})
+onMounted(() => {
 
-const count = ref(0)
-</script>
-
-<template>
-
-<div>
-  <div id="bird" style="width: 100vw; height: 100vh;">
-  <h1>{{ msg }}</h1>
-  </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.birds.min.js"></script>
-<script>
 VANTA.BIRDS({
   el: "#bird",
   mouseControls: true,
@@ -27,9 +15,26 @@ VANTA.BIRDS({
   scale: 1.00,
   scaleMobile: 1.00
 })
-</script></div>
-  
 
+})
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+
+
+</script>
+
+
+<template>
+<div id="bird" style="width:100vw;height:100vh">
+
+    <h1>{{ msg }}</h1>
+
+</div>
 </template>
 
 <style scoped>
