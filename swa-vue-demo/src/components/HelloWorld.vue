6b6a8b80623dfa9da5d89bb4c962da340a6c24bc
kathyhana <kathy@yaku.icu>
2024-09-07 19:55:46 +0800

    更新 HelloWorld.vue

--- swa-vue-demo/src/components/HelloWorld.vue
+++ swa-vue-demo/src/components/HelloWorld.vue
@@ -11,7 +11,16 @@ VANTA.BIRDS({
   minHeight: 200.00,
   minWidth: 200.00,
   scale: 1.00,
-  scaleMobile: 1.00
+  scaleMobile: 1.00,
+  backgroundColor: 0xf0f0f0,
+  color1: 0x0,
+  color2: 0xef003b,
+  birdSize: 1.60,
+  wingSpan: 16.00,
+  separation: 94.00,
+  alignment: 88.00,
+  cohesion: 25.00,
+  backgroundAlpha: 0.57
 })
 
 }
